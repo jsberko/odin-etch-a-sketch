6033e0f39e10c8fe41b57e6841e5fb5c6ad875ac
jsberko <147778552+jsberko@users.noreply.github.com>
2024-11-12 13:44:41 -0500

    Created index.html, style.css, and javascript.js

--- javascript.js
+++ javascript.js
@@ -1,55 +1,37 @@
+// Selectors
 const container = document.querySelector(".container");
 const gridSizeButton = document.querySelector("#gridSize");
 
-// let gridSize = 0;
 
-for (let i = 1; i <= 16; i++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-
-    square.style.width = "239px";
-    square.style.height = "239px";
-
-    container.append(square);
-
-    square.addEventListener("mouseenter", (event) => {
-        event.target.classList.toggle("highlight");
-    })
-
-    square.addEventListener("mouseleave", (event) => {
-        event.target.classList.toggle("highlight");
-    })
-}
 
 
 // Functions
-
 function getGridSize() {
-    console.log("getGridSize called");
     let userInput = parseInt(prompt("Enter a number between 1-100"));
     let gridSize = userInput * userInput;
 
     while (isNaN(userInput) || !userInput || userInput < 1 || userInput > 100) {
         userInput = prompt("Invalid input. Please enter a number between 1-100");
     }
-    console.log(userInput, gridSize);
+
     getSquareSize(userInput, gridSize);
 }
 
+
 function getSquareSize(userInput, gridSize) {
-    console.log("getSquareSize called");
     let squareSize = 956 / userInput
+
     resetGrid(squareSize, gridSize);
 }
 
+
 function resetGrid(squareSize, gridSize) {
-    console.log("resetGrid called");
     removeAllChildren(container);
     buildGrid(squareSize, gridSize);
 }
 
+
 function buildGrid(squareSize, gridSize) {
-    console.log("buildGrid called");
     for (let i = 1; i <= gridSize; i++) {
         const square = document.createElement("div");
         square.classList.add("square");
@@ -60,24 +42,13 @@ function buildGrid(squareSize, gridSize) {
         container.append(square);
 
         square.addEventListener("mouseenter", (event) => {
-            event.target.classList.toggle("hover");
+            event.target.classList.add("hover");
         })
-
-        square.addEventListener("mouseleave", (event) => {
-            event.target.classList.toggle("hover");
-        })
-
-        if (i === gridSize) {
-            console.log("container.style.width");
-            console.log("container.style.height");
-            console.log("square.style.width");
-            console.log("square.style.height");
-        }
     }
 }
 
+
 function removeAllChildren(parent) {
-    console.log("removeAllChildren called");
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
     }
@@ -85,19 +56,13 @@ function removeAllChildren(parent) {
 
 
 
+
 // Event listeners
-
-container.addEventListener
-
 gridSizeButton.addEventListener("click", () => {
-    //add contraints/validations
-    // num = +prompt("Please enter a number between 1-100");
-    // gridSize = num * num;
-    // resetGrid();
     getGridSize();
 })
 
 
-// function changeColor() {
-//     square.style.color = "black";
-// }
+addEventListener("load", (event) => {
+    buildGrid(239, 16);
+});
